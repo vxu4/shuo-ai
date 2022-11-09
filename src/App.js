1ce9ec7475cf4f305b65e900a76278c9d47ae64f
vxu4 <vixixu68@gmail.com>
2022-11-09 07:21:33 -0800

    adding background text

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import './App.css';
 import React, { useState, createContext } from 'react';
 import Photo from './components/photo';
+import OG from './assets/OG.png'
 
 export const AppContext = createContext();
 
@@ -13,11 +14,24 @@ function App() {
         <div 
         className="overlay-content" 
         style={{  
-          background: display ? "rgba(255, 255, 255, 0)" : "rgba(255, 255, 255, 0.5)",
+          background: display ? "rgba(255, 255, 255, 0)" : "rgba(255, 255, 255, 0.4)",
           // opacity: display ? 0 : 1,
-          display: display ? 'none' : 'inline'
+          display: display ? 'none' : 'flex'
+        }}> 
+          {/* When I say Ai, I feel <button onClick={() => setDisplay(true)}>here</button> */}
+          <img src={OG} style={{width: '720px', height: '520px'}}></img>
+        </div>
+        <div 
+        className="overlay-content" 
+        style={{  
+          background: display ? "rgba(255, 255, 255, 0)" : "rgba(255, 255, 255, 0)",
+          // opacity: display ? 0 : 1,
+          display: display ? 'none' : 'flex',
+          color: "white",
+          fontWeight: 700
         }}> 
           When I say Ai, I feel <button onClick={() => setDisplay(true)}>here</button>
+          {/* <img src={OG} style={{width: '720px', height: '520px'}}></img> */}
         </div>
         <Photo />
       </div>
