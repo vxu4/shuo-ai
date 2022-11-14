5e777cbdc3c36362d9a69a5f9f8c1f588c7e2e7c
vxu4 <vixixu68@gmail.com>
2022-11-14 12:27:21 -0800

    removing vid imports to test gh-pages

--- src/components/rows/vidSrcMap.js
+++ src/components/rows/vidSrcMap.js
@@ -1,32 +1,31 @@
-import src from '../../assets/13px.mp4';
-import vid01 from '../../assets/01_IMG0525.mp4';
-import vid02 from '../../assets/02_IMG0641.mp4';
-import vid03 from '../../assets/03_IMG0359.mp4';
-import vid04 from '../../assets/04_IMG0417.mp4';
-import vid05 from '../../assets/05_IMG0428.mp4';
-import vid06 from '../../assets/06_IMG0442.mp4';
-import vid07 from '../../assets/07_IMG0485.mp4';
-import vid08 from '../../assets/08_IMG0540.mp4';
-import vid09 from '../../assets/09_IMG0541.mp4';
-import vid10 from '../../assets/10_IMG0543.mp4';
-import vid11 from '../../assets/11_IMG0655.mp4';
-import vid12 from '../../assets/12_IMG0817.mp4';
-import vid13 from '../../assets/13_IMG0825.mp4';
+// import vid01 from '../../assets/01_IMG0525.mp4';
+// import vid02 from '../../assets/02_IMG0641.mp4';
+// import vid03 from '../../assets/03_IMG0359.mp4';
+// import vid04 from '../../assets/04_IMG0417.mp4';
+// import vid05 from '../../assets/05_IMG0428.mp4';
+// import vid06 from '../../assets/06_IMG0442.mp4';
+// import vid07 from '../../assets/07_IMG0485.mp4';
+// import vid08 from '../../assets/08_IMG0540.mp4';
+// import vid09 from '../../assets/09_IMG0541.mp4';
+// import vid10 from '../../assets/10_IMG0543.mp4';
+// import vid11 from '../../assets/11_IMG0655.mp4';
+// import vid12 from '../../assets/12_IMG0817.mp4';
+// import vid13 from '../../assets/13_IMG0825.mp4';
 
 export const vidSrcMap = [
-  vid01,
-  vid02,
-  vid03,
-  vid04,
-  vid05,
-  vid06,
-  vid07,
-  vid08,
-  vid09,
-  vid10,
-  vid11,
-  vid12,
-  vid13
+  // vid01,
+  // vid02,
+  // vid03,
+  // vid04,
+  // vid05,
+  // vid06,
+  // vid07,
+  // vid08,
+  // vid09,
+  // vid10,
+  // vid11,
+  // vid12,
+  // vid13
   ]
 ;
 
