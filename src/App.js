97e7767462c88cb9c8c8ff4f72167dc8e250d6d8
vxu4 <45617317+vxu4@users.noreply.github.com>
2022-11-17 11:46:45 -0500

    adding message fenerator" ;

--- src/App.js
+++ src/App.js
@@ -8,6 +8,16 @@ export const AppContext = createContext();
 function App() {
   const [display, setDisplay] = useState(false);
   const [remember, setRemember] = useState(false);
+  const transmission = () => {
+    if (remember) {
+      return('What is found and left in the tracing?')
+    }
+    else if (display) {
+      return('Transmission: Do you remember the strokes I taught you? Start at the top right corner. Hover. Go - write love into lost pixels. But don`t forget, listen to the presence within')
+    } else {
+      return('IMG_33-2x')
+    }
+  }
 
   return (
     <AppContext.Provider value={{ display, remember }}>
@@ -43,7 +53,7 @@ function App() {
         </div>
         <div style={{ width: 'auto' }} className="window">
           <div className="title-bar">
-            <div className="title-bar-text">{display ? 'Transmission: Do you remember the strokes I taught you? Start at the top right corner. Hover. Go - write love into lost pixels. But don`t forget, listen to the presence within' : 'IMG_33-2x'}</div>
+            <div className="title-bar-text">{transmission()}</div>
             <div className="title-bar-controls" style={{ margin: '0px' }}>
               {
                 display && !remember &&
@@ -51,7 +61,7 @@ function App() {
                   aria-label="Close"
                   onClick={() => setRemember(true)}
                   style={{ cursor: 'pointer', margin: '0px', paddingLeft: '15px', paddingRight: '5px', paddingBottom: '2px' }}>
-                  {display ? 'remembrance' : ''}
+                  remembrance
                 </button>
               }
               {
@@ -60,7 +70,7 @@ function App() {
                   // aria-label="Close"
                   onClick={() => {setRemember(false); setDisplay(false)}}
                   style={{ cursor: 'pointer', margin: '0px', paddingLeft: '15px', paddingRight: '5px', paddingBottom: '2px' }}>
-                  {display ? 'repeat' : ''}
+                  repeat
                 </button>
               }
             </div>
